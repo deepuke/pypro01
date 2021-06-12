78df99cc48feb0ba2f4817ecdf72d74a3a900740
deepuke <dipuke@gmail.com>
2021-06-13 00:38:09 +0530

    Video API structure added

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, abort, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video, required arg", required=True)
+video_put_args.add_argument("likes", type=str, help="Likes of the video, required arg", required=True)
+video_put_args.add_argument("views", type=str, help="Views on the video, required arg", required=True)
 
-# class 'HellowWorld' inherit from 'Resource'
-class HelloWorld(Resource):
-    # override the get method
-    def get(self):
-        #python library [json in javascript]
-        return {'hello' : 'Hello world'}
-    # override the post method
-    def post(self):
-        return {'data': 'Posted'}
-    
+videos = {}
 
+def abort_if_not_exist(video_id):
+    if video_id not in videos:
+        abort(404, message="Video doesnt found!")
 
-class PassingParams(Resource):
-    def get(self, name, age):
-        return {'name':name, 'age':age}
+def abort_if_already_exist(video_id):
+    if video_id in videos:
+        abort(409, message="Video already exists with the same id!")
+
+class Video(Resource):
+    def get(self, video_id):
+        abort_if_not_exist(video_id)
+        return videos[video_id]
+
+    def put(self, video_id):
+        abort_if_already_exist(video_id)       
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id]
+
+    def delete(self, video_id):
+        abort_if_not_exist(video_id)
+        del videos[video_id]        
+        return '', 204
 
 #add 'HelloWorld' class to the api 'resource'
-api.add_resource(HelloWorld, '/helloworld')
-api.add_resource(PassingParams, '/pp/<string:name>/<int:age>') 
+api.add_resource(Video, '/video/<int:video_id>')
 
 if __name__ == "__main__":
     # debug mode for development purpose
--- test.py
+++ test.py
@@ -1,9 +1,22 @@
+from flask.globals import request
 import requests
 
 BASE = "http://127.0.0.1:5000/"
-# response = requests.get(BASE+"helloworld")
-response = requests.post(BASE+"helloworld")
-print(response.json())
 
-response = requests.get(BASE+"pp/Deepu/37")
+data = [
+    {'views': 110, 'name':'Tim', 'likes':65}, 
+    {'views': 5410, 'name':'abc', 'likes':690}, 
+    {'views': 120, 'name':'pqr', 'likes':2316}
+]
+
+for i in range(len(data)):
+    response = requests.put(BASE+"video/"+ str(i), data[i])
+    print(response.json())
+
+response = requests.delete(BASE+"video/0")
+print(f"{response.status_code} and {response}")
+
+input()
+
+response = requests.get(BASE+"video/2")
 print(response.json())
